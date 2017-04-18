1f55b13035182d685e21b304879a3de1f011e54d
smab90@Apofis_smab <smab90@mail.ru>
2017-04-19 01:15:58 +0300

    er

---
+++ src/er_diagram.h
@@ -0,0 +1,88 @@
+#ifndef ER_DIAGRAM_H
+#define ER_DIAGRAM_H
+#include <string>
+#include <vector>
+template <typename T>
+class er
+{
+  private:
+
+  struct relation{
+                std::string type;
+                er *other;
+            };
+  struct atribut{
+                std::string name;
+                T value;
+            };
+  std::vector<relation> *relations;
+  std::vector<atribut> *atributs;
+  int i_at;
+  int i_re;
+ public:
+  er()
+  {
+      i_at=0;
+      i_re=0;
+      relations=new std::vector<relation>;
+      atributs=new std::vector<atribut>;
+  }
+
+  ~er() {
+     if (relations)
+        delete[] relations;
+     if (atributs)
+        delete[] atributs;
+  }
+
+  int insert_atribut(std::string name,const T value)
+  {
+    atributs->push_back(atribut());
+    atributs->at(i_at).name=name;
+    atributs->at(i_at).value=value;
+    i_at++;
+    return 0;
+  }
+
+  int insert_relatin(std::string type,er *other)
+  {
+    relations->push_back(relation());
+    relations->at(i_re).type=type;
+    relations->at(i_re).other=other;
+    i_re++;
+    return 0;
+  }
+
+  int return_amount_atributs()
+  {
+      return i_at;
+  }
+
+  int return_amount_relations()
+  {
+      return i_re;
+  }
+
+  std::string return_atribut_name(int i)
+  {
+      return atributs->at(i).name;
+  }
+
+  T return_atribut_value(int i)
+  {
+      return atributs->at(i).value;
+  }
+
+  er* return_relation(int i)
+  {
+      return relations->at(i).other;
+  }
+
+  std::string return_relation_type(int i)
+  {
+      return relations->at(i).type;
+  }
+};
+
+
+#endif // ER_DIAGRAM_H
